de48a6969bb488c111ab8a479970ef56e15e5555
PhilippProzorov <43746355+PhilippProzorov@users.noreply.github.com>
2019-09-18 22:21:17 +0300

    Minor fixes

--- src/game/controllers/MenuController.java
+++ src/game/controllers/MenuController.java
@@ -78,8 +78,6 @@ public class MenuController {
     @FXML
     void startGame() {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("fxml/Game.fxml"));
             gameTitle.getScene().getWindow().hide();
             GameController.setSize(tilesPreset);
             GameController.setMines(minesPreset);
@@ -91,23 +89,23 @@ public class MenuController {
 
     @FXML
     void rules() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        ButtonType cancel = new ButtonType("Return", ButtonBar.ButtonData.OK_DONE);
-        alert.getButtonTypes().setAll(cancel);
-        alert.setTitle(" ");
-        alert.setHeaderText("Basics of the game");
-        alert.setContentText("1) You are presented with a board of hexagons. Some of them contain mines, others " +
-                "don't. If you click on a hex containing a mine, you lose. If you manage to open all " +
-                "the hex (without clicking on any mines) you win.\n" + "\n" +
-                "2) Clicking a tile which doesn't have a mine reveals the number of neighbouring tiles " +
-                "containing mines. Use this information plus some guess work to avoid the mines.\n" + "\n" +
-                "3) To open a tile, point at the hex and click on it. To mark a tile you think is a mine, " +
-                "point and right-click.");
-        Optional<ButtonType> actions = alert.showAndWait();
-        if (actions.get() == cancel) {
-            alert.close();
-        }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            ButtonType cancel = new ButtonType("Return", ButtonBar.ButtonData.OK_DONE);
+            alert.getButtonTypes().setAll(cancel);
+            alert.setTitle(" ");
+            alert.setHeaderText("Basics of the game");
+            alert.setContentText("1) You are presented with a board of hexagons. Some of them contain mines, others " +
+                    "don't. If you click on a hex containing a mine, you lose. If you manage to open all " +
+                    "the hex (without clicking on any mines) you win.\n" + "\n" +
+                    "2) Clicking a tile which doesn't have a mine reveals the number of neighbouring tiles " +
+                    "containing mines. Use this information plus some guess work to avoid the mines.\n" + "\n" +
+                    "3) To open a tile, point at the hex and click on it. To mark a tile you think is a mine, " +
+                    "point and right-click.\n" + "\n" +
+                    "• To start a new game while in-game, press the smiley face.");
+            Optional<ButtonType> action = alert.showAndWait();
+            if (action.equals(cancel)) alert.close();
     }
+
 
     @FXML
     void exitGame() {
